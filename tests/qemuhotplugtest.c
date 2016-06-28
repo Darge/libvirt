f935f73379f42a59797c107c94c009d45eb71618
Tomasz Flendrich <Darge@users.noreply.github.com>
2016-06-28 08:38:03 +0000

    segfault

--- tests/qemuhotplugtest.c
+++ tests/qemuhotplugtest.c
@@ -523,6 +523,13 @@ mymain(void)
                    "device_del", QMP_OK,
                    "chardev-remove", QMP_OK);
 
+    DO_TEST_ATTACH("hotplug-base-ccw-live", "mine-modifications", false, true,
+                   "chardev-add", QMP_OK,
+                   "device_add", QMP_OK);
+    DO_TEST_DETACH("hotplug-base-ccw-live", "mine-modifications", false, false,
+                   "device_del", QMP_OK,
+                   "chardev-remove", QMP_OK);
+
     qemuTestDriverFree(&driver);
     return (ret == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
