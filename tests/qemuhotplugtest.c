67a14028c9c2fdd604a25921cd6dfdf3ae27cffc
Tomasz Flendrich <t.flendrich@gmail.com>
2016-07-01 00:14:10 +0200

    use less files in tests

--- tests/qemuhotplugtest.c
+++ tests/qemuhotplugtest.c
@@ -527,7 +527,7 @@ mymain(void)
     DO_TEST_ATTACH("hotplug-base-ccw-live", "ccw-virtio", false, true,
                    "human-monitor-command", HMP("OK\\r\\n"),
                    "device_add", QMP_OK);
-    DO_TEST_DETACH("hotplug-base-ccw-live-with-ccw-virtio", "ccw-virtio", false, false,
+    DO_TEST_DETACH("hotplug-base-ccw-live", "ccw-virtio", false, false,
                    "device_del", QMP_OK,
                    "chardev-remove", QMP_OK);
 
