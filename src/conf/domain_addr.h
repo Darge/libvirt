7de9b545fe7162307529f0cd67e83e35923412d2
Tomasz Flendrich <t.flendrich@gmail.com>
2016-07-19 20:02:04 +0200

    Move and rename qemuDomainValidateDevicePCISlotsQ35

--- src/conf/domain_addr.h
+++ src/conf/domain_addr.h
@@ -250,6 +250,12 @@ virDomainValidateDevicePCISlotsPIIX3(virDomainDefPtr def,
                                       bool videoPrimaryEnabled)
     ATTRIBUTE_NONNULL(1) ATTRIBUTE_NONNULL(2);
 
+int
+virDomainValidateDevicePCISlotsQ35(virDomainDefPtr def,
+                                    virDomainPCIAddressSetPtr addrs,
+                                    bool videoPrimaryEnabled)
+    ATTRIBUTE_NONNULL(1) ATTRIBUTE_NONNULL(2);
+
 
 
 #endif /* __DOMAIN_ADDR_H__ */
