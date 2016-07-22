629a450a6bba241a2a46c498f4d9c8abb5751e16
Tomasz Flendrich <t.flendrich@gmail.com>
2016-07-22 22:47:56 +0200

    qemu_hotplug: generate pci address list on demand
    Dropping the caching of pci address set.
    Instead of using the cached address set, functions in qemu_hotplug.c
    now recalculate it on demand.
    
    Conflicts:
    	src/qemu/qemu_domain_address.c

--- src/qemu/qemu_domain_address.c
+++ src/qemu/qemu_domain_address.c
@@ -941,7 +941,7 @@ qemuDomainValidateDevicePCISlotsChipsets(virDomainDefPtr def,
 }
 
 
-static virDomainPCIAddressSetPtr
+virDomainPCIAddressSetPtr
 qemuDomainPCIAddressSetCreate(virDomainDefPtr def,
                               virQEMUCapsPtr qemuCaps,
                               bool dryRun,
@@ -1836,12 +1836,6 @@ qemuDomainReleaseDeviceAddress(virDomainObjPtr vm,
 
     if (!devstr)
         devstr = info->alias;
-
-    if (virDeviceInfoPCIAddressPresent(info) &&
-        virDomainPCIAddressReleaseSlot(priv->pciaddrs,
-                                       &info->addr.pci) < 0)
-        VIR_WARN("Unable to release PCI address on %s",
-                 NULLSTR(devstr));
 
     if (info->type == VIR_DOMAIN_DEVICE_ADDRESS_TYPE_USB &&
         priv->usbaddrs &&
